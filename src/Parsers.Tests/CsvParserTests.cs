19d1ef61e5e604b2541546c4c2ce751abf232ee9
Dale Ragan <dale@ragan.io>
2013-12-26 16:10:56 -0500

    Parsing fields, just need to remove quotes.

--- src/Parsers.Tests/CsvParserTests.cs
+++ src/Parsers.Tests/CsvParserTests.cs
@@ -6,7 +6,8 @@ using System.Text.RegularExpressions;
 
 public class CsvParserTests
 {
-    string test_string;
+    readonly string test_string;
+    readonly string[][] expected_output;
 
     public CsvParserTests ()
     {
@@ -17,19 +18,57 @@ public class CsvParserTests
                       "zzz,yyy,xxx\r\n" + 
                       "1,,3\r\n" + 
                       ",,";
+
+        expected_output = new string[][] { 
+            new [] { "field1", "field2", "field3" },
+            new [] { "aaa\r\n", "bb,b", "ccc" },
+            new [] { "in \"quotes\"", "2", "3" },
+            new [] { "1", "2", "" },
+            new [] { "zzz", "yyy", "xxx" },
+            new [] { "1", "", "3" },
+            new [] { "", "", "" }
+        };
     }
 
     public void should_contain_seven_lines ()
     {
-        Parse (test_string).Count.ShouldEqual (7);
+        Parse (test_string).Count.ShouldEqual (expected_output.Length);
     }
 
-    public static List<string> Parse (string input)
+    public void each_line_should_contain_three_columns ()
     {
-        var output = new List<string> ();
+        var expectedFieldCount = 3;
+        var output = Parse (test_string);
+
+        foreach (var line in output) {
+            line.Count.ShouldEqual (expectedFieldCount);
+        }
+    }
+
+    public void each_line_should_contain_expected_field ()
+    {
+        var output = Parse (test_string);
+
+        for (int i = 0; i < expected_output.Length; i++) {
+            for (int j = 0; j < expected_output[i].Length; j++) {
+                output [i] [j].ShouldEqual (expected_output [i] [j]);
+            }
+        }
+    }
+
+    public static List<List<string>> Parse (string input)
+    {
+        var output = new List<List<string>> ();
 
         foreach (string line in Regex.Split (input, @"\r\n(?=(?:[^""]*""[^""]*"")*[^""]*$)")) {
-            output.Add (line);
+
+            var fields = new List<string> ();
+
+            foreach (string field in Regex.Split (line, @"\s*,\s*(?=(?:[^""]*""[^""]*"")*[^""]*$)")) {
+                fields.Add (field);
+            }
+
+            output.Add (fields);
         }
 
         return output;
